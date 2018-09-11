c485a44c37637d4fedf5df77d88521b9c86656c8
James Hristu <jhristu@gmail.com>
2018-09-10 22:49:14 -0700

    Kinda working...

--- page_function.js
+++ page_function.js
@@ -1,21 +1,45 @@
-// find elements
-var preText = $("pre").text();
-var lines = preText.split("\n");
-var citiesData = Array();
+function pageFunction(context) {
+    // trim polyfill
+    if (!String.prototype.trim) {
+        String.prototype.trim = function () {
+          return this.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+        };
+      }
 
-for (i = 0; i < lines.length; i++) {
-    var line = lines[i];
-    if (line.startsWith("CITY")) {
-        console.log("CITY at " + i);
-        // TODO: Find columnar locations
+    var $ = context.jQuery;
 
-        var cityIndices = Array();
+    // find elements
+    var preText = $("pre").text();
+    var lines = preText.split("\n");
+    var citiesData = Array();
 
-        while (!lines[++i].startsWith("$$")) {
-            line = lines[i];
-            citiesData.push(line);
+    for (i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        if (line.startsWith("CITY")) {
+            var labels = line.split(/\s+/).filter(Boolean);
+            for (var j = 0; j < labels.length; j++) {
+                labels[j] = [labels[j], line.indexOf(labels[j])];
+            }
+
+            while (!lines[++i].startsWith("$$")) {
+                line = lines[i];
+                var cityData = {};
+                for (var k = 0; k < labels.length; k++) {
+                    if (k == labels.length - 1) {
+                        citiyValue = line.slice[labels[k][1]];
+                    } else {
+                        cityValue = line.slice(labels[k][1], labels[k+1][1]).trim();
+                    }
+
+                    if (cityValue !== undefined) {
+                        cityValue.trim();
+                        cityData[labels[k][0]] = cityValue;
+                    }
+                }
+                citiesData.push(cityData);
+            }
         }
     }
-}
 
-console.log(citiesData)
+    return citiesData;
+}
